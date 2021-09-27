bde4ab7a555081c04f21245fcef40fec8068aadd
Documenter.jl <documenter@juliadocs.github.io>
2021-09-27 12:47:01 +0000

    build based on 36a295a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BayesNegativeBinomial","category":"page"},{"location":"#BayesNegativeBinomial","page":"Home","title":"BayesNegativeBinomial","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BayesNegativeBinomial.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BayesNegativeBinomial]","category":"page"}]
+}
