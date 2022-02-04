78275fd92bf03af32299677e98c868b284a26d4b
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2022-02-04 14:06:39 +0000

    Check that the routine works with non-trivial mappings

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,19 +6,23 @@ using Test
 function simulate_sample(rng, N, D)
     β = - 0.5 * ones(D)
     X = rand(N, D)
-    γ = ones(Bool, D)
+    γ = ones(Bool, 3)
     γ[2] = false
-    γ[4] = false
-    p = 1 ./ (1 .+ exp.(- X[:, γ] * β[γ]))
+    mapping = [[1], collect(2:D-1), [D]]
+    γexp = zeros(Bool, D)
+    for d in 1:length(mapping)
+        γexp[mapping[d]] .= γ[d]
+    end
+    p = 1 ./ (1 .+ exp.(- X[:, γexp] * β[γexp]))
     y = [rand(rng, NegativeBinomial(2, 1 - p[i])) for i in 1:N]
-    return p, y, X
+    return p, y, X, mapping
 end
 
 @testset "BayesNegativeBinomial.jl" begin
     N, D = 2000, 4
     rng = MersenneTwister(1)
-    p, y, X = simulate_sample(rng, N, D)
-    sampler = BayesNegativeBinomial.Sampler(y, X)
+    p, y, X, mapping = simulate_sample(rng, N, D)
+    sampler = BayesNegativeBinomial.Sampler(y, X; mapping)
     BayesNegativeBinomial.step!(rng, sampler)
     sampler = BayesNegativeBinomial.Sampler(y, X)
     chain = BayesNegativeBinomial.sample(rng, sampler)
